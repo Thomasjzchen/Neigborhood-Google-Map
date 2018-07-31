5586ca330caaf86aa864aca07c6cf510a623d670
jizhi chen <chenjizhi@hotmail.com>
2018-07-31 11:56:02 +0800

    update the project

---
+++ src/locations.js
@@ -0,0 +1,41 @@
+export const allLocation = [{
+  location: {
+    lat: 23.128224,
+    lng: 113.260111,
+  },
+  title: 'Temple of the Six Banyan Trees',
+  address: '87 Liurong Rd, ZhongShan Liu Lu, Yuexiu Qu, Guangzhou Shi, Guangdong Sheng, China, 510000',
+},
+{
+  location: {
+    lat: 23.129501,
+    lng: 113.256299
+  },
+  title: 'Guangxiao Temple (Guangzhou)',
+  address: '109 Guangxiao Rd, Yuexiu Qu, Guangzhou Shi, Guangdong Sheng, China, 510000'
+},
+{
+  location: {
+    lat: 23.120463,
+    lng: 113.259836
+  },
+  title: 'Temple of the Five Immortals (Guangzhou)',
+  address: 'Yuexiu, Guangzhou, Guangdong, China, 510000'
+},
+{
+  location: {
+    lat: 23.133502,
+    lng: 113.264731
+  },
+  title: 'Sun Yat-sen Memorial Hall (Guangzhou)',
+  address: 'Yuexiu, Guangzhou, China, 510000'
+},
+{
+  location: {
+    lat: 23.137688,
+    lng: 113.261489
+  },
+  title: 'Museum of the Mausoleum of the Nanyue King',
+  address: '867 Jiefang N Rd, Yuexiu Qu, Guangzhou Shi, Guangdong Sheng, China, 510000'
+}
+];
